20381569403786283b396ab6ad80cf788610f3ef
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2022-09-20 19:40:27 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,7 @@
+// Start dynamic PDF with mail sent 
+require_once get_theme_file_path('/dynamic-pdf/config.php');
+require_once get_theme_file_path('/dynamic-pdf/vendor/autoload.php');
+require_once get_theme_file_path('/dynamic-pdf/class.pdf.php');
+require_once get_theme_file_path('/dynamic-pdf/product-pdf.php');
+// End dynamic PDF with mail sent 
+
